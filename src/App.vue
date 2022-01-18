90ecabb342b98b8af8ee8dc9b2946b3ce5f0ed86
KaimNotark <kaimnotark@yandex.ru>
2022-01-18 13:00:27 +0800

    attach fonts and styles

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+
+@import "./stylesheets/variables.scss";
+@import "./stylesheets/resets.scss";
+
+$font-family-primary: "Roboto", "Verdana", "Arial", sans-serif;
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-family-primary;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
